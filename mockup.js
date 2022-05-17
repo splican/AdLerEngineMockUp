dd6d3497fb7ec98ebed5feaef8c7c9138527758e
splican <daniel2105@gmx.net>
2022-05-17 08:27:12 +0200

    menu with js

--- mockup.js
+++ mockup.js
@@ -1,3 +1,21 @@
-let leftMenu = document.getElementById("mainMenu");
+let HamburgerMenu = document.getElementById("HamburgerMenu");
+let LearningElementMenu = document.getElementById("LearningElementMenu");
 
-leftMenu.style.display = "none";
+let HamburgerMenuOff = (HamburgerMenu.style.display = "none");
+let LearningElementMenuOff = (LearningElementMenu.style.display = "none");
+
+function HamburgerMenuSwitch() {
+  if (HamburgerMenu.style.display == "none") {
+    HamburgerMenu.style.display = "block";
+  } else {
+    HamburgerMenu.style.display = "none";
+  }
+}
+
+function LearningElementMenuSwitch() {
+  if (LearningElementMenu.style.display == "none") {
+    LearningElementMenu.style.display = "block";
+  } else {
+    LearningElementMenu.style.display = "none";
+  }
+}
